561561202f8dd5707438516dd1a3bc9b1f24bc50
kevin <621032825@qq.com>
2019-04-23 17:51:13 +0800

    推入框架

---
+++ flask/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import make_response
+import json
+
+app = Flask(__name__)
+
+a = json.dumps({
+        "a": '''<p onclick="alert(window.cookie)">111</p>
+               <a href="https://www.baidu.com">超链接</a>'''
+    })
+
+@app.route('/')
+def hello_world():
+    rst = make_response(a)
+    rst.headers['Access-Control-Allow-Origin'] = '*'
+    # rst.headers['Access-Control-Allow-Credentials'] = True
+    return rst
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port='5052', host='0.0.0.0')
