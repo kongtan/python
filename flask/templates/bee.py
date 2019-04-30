7ea288c0e97e19dfa525788c3ef18c8eab92b8b6
Kevin <lzy12611@ly.com>
2019-04-30 18:05:29 +0800

    1

--- flask/templates/bee.py
+++ flask/templates/bee.py
@@ -1,9 +1,17 @@
 from selenium import webdriver
+import pymysql
+import re
 import json
 
 driver = webdriver.PhantomJS(executable_path=r'F:\phantomjs-2.1.1-windows\bin\phantomjs.exe')
 source = []
 
+
+# 打开数据库连接
+# db = pymysql.connect("localhost", "root", "123456", "beedata")
+
+# 使用 cursor() 方法创建一个游标对象 cursor
+# cursor = db.cursor()
 
 def getMovie():
     global source
@@ -13,5 +21,17 @@ def getMovie():
     # print(driver.page_source)
     arr = []
     for item in source:
-        arr.append(item.get_attribute('innerHTML'))
+        types = re.search(r'\w*\/', item.find_element_by_css_selector('.mod-tit a').get_attribute('innerHTML'),
+                          re.I | re.M).group()
+        for itemSon in item.find_elements_by_css_selector('.update_area_lists .i_list'):
+            arr.append({
+                'type': types,
+                'title': re.search(r'《\w*》', itemSon.find_element_by_css_selector('h2').get_attribute('innerHTML'),
+                                   re.I | re.M).group(),
+                'videoId': re.search(r'\d+', itemSon.find_element_by_css_selector('a').get_attribute('href'),
+                                     re.I | re.M).group()
+            })
     return json.dumps(arr)
+
+# 关闭数据库连接
+# db.close()
